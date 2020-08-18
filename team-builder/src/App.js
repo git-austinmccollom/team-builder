95d25403e0ca232801fae90681b1e6be67849efc
git-austinmccollom <austinmccollom@gmail.com>
2020-08-18 18:42:39 -0500

    mapped array of divs for each team member from state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -5,13 +5,16 @@ import Form from './Components/Form'
 import TeamMember from './Components/TeamMember'
 
 function App() {
-  // const [team]
+  const [ team, setTeam ] = useState([{id: 1, name: 'Austin McCollom', email: 'austinmccollom@gmail.com', role: 'Full-Stack Web Developer'}, {id: 2, name: 'Sam Hill', email: 'samuelgarretthill@gmail.com', role: 'Full-Stack Web Developer'}]);
   return (
     <div className="App">
       <h1>Team</h1>
       <Form />
-      <TeamMember />
-
+      {
+        team.map( member => {
+          return <TeamMember key={member.id} member={member} />
+        })
+      }
     </div>
   );
 }
